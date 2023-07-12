cecace13adb1b3b59aaabb884e15cef21a94ef51
SauravBeginner <kundusaurav1996@gmail.com>
2023-07-12 15:04:53 +0530

    push

---
+++ src/admin/app.example.js
@@ -0,0 +1,75 @@
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
+
+//   config: {
+//     // Override or extend the theme
+//     theme: {
+//       colors: {
+//         alternative100: "#f6ecfc",
+//         alternative200: "#e0c1f4",
+//         alternative500: "#ac73e6",
+//         alternative600: "#9736e8",
+//         alternative700: "#8312d1",
+//         danger700: "#b72b1a",
+//       },
+//     },
+//     // // Extend the translations
+//     translations: {
+//       en: {
+//         "app.components.LeftMenu.navbrand.title": "Publishpen",
+//         "app.components.LeftMenu.navbrand.workplace": "Dashboard",
+//         "Auth.form.welcome.title": "dgfd!!!:)",
+//       },
+//       fr: {
+//         "Auth.form.email.label": "test",
+//         Users: "Utilisateurs",
+//         City: "CITY (FRENCH)",
+//         // Customize the label of the Content Manager table.
+//         Id: "ID french",
+//       },
+//     },
+//     // Disable video tutorials
+//     tutorials: false,
+//     // Disable notifications about new Strapi releases
+//     // notifications: { release: false },
+//   },
+
+//   bootstrap() {},
+// };
